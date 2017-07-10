023424d53c90b909a6d1b1248e3ed3cf5db6d900
Karthikaveni <karthikaveni18@gmail.com>
2017-07-10 17:38:15 +0530

    Create hunter13 .java

---
+++ hunter13 .java
@@ -0,0 +1,57 @@
+# hm
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class hunter13 {
+    public static void main(String[] args)
+    {
+        int n;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        List<String> list=new LinkedList<String>();
+        System.out.println("Enter the number of character");
+        try {
+            n=Integer.parseInt(br.readLine());
+            System.out.println("Enter the Character");
+            for(int i=0;i<n;i++)
+            {
+
+                list.add(br.readLine());
+            }
+boolean palindrome=false;
+for(int j=list.size()-1,i=0;i<list.size()&&j>=0&&i!=j;i++,j--)
+{
+if(list.get(i).equals(list.get(j)))
+{
+palindrome=true;
+
+}
+    else
+{
+    palindrome=false;
+    break;
+}
+}
+            System.out.println(palindrome==true?"List is palindrome":"List is not palindrome");
+
+
+
+
+
+        }
+        catch(Exception e)
+        {
+e.printStackTrace();
+        }
+
+    }
+    class Node
+    {
+        public String character;
+
+    }
+
+}
